7cffca5fbdcfff71579b67adbd91f1bdf157c403
louis <louis.pepin.ce@gmail.com>
2016-02-19 16:33:43 -0500

    generateclientid, create, list

--- code_tp1/src/ca/polymtl/inf4410/tp1/server/Server.java
+++ code_tp1/src/ca/polymtl/inf4410/tp1/server/Server.java
@@ -8,6 +8,12 @@ import java.rmi.server.UnicastRemoteObject;
 
 import ca.polymtl.inf4410.tp1.shared.ServerInterface;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
 public class Server implements ServerInterface {
 
 	public static void main(String[] args) {
@@ -39,6 +45,22 @@ public class Server implements ServerInterface {
 		} catch (Exception e) {
 			System.err.println("Erreur: " + e.getMessage());
 		}
+
+		try {
+			System.out.println("New id: " + generateclientid());
+			if (create("test.txt")) {
+				System.out.println("New file");
+			}
+			else {
+				System.out.println("File already exists");
+			}
+			System.out.println("List: ");
+			System.out.println(list());
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+
 	}
 
 	/*
@@ -57,5 +79,67 @@ public class Server implements ServerInterface {
 	@Override
 	public void execute(byte[] arg) throws RemoteException {
 		return;
+	}
+
+	/*
+	 * Trouve le plus grand id dans 'id.txt' et en génère un plus grand  
+	 */
+	public int generateclientid() throws Exception {
+		File file = new File("id.txt");
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+
+		// Generate new ID
+		String line; int new_id = 0;
+		while ((line = reader.readLine()) != null) {
+			int current_id = Integer.parseInt(line);
+			if(current_id > new_id) {
+				new_id = current_id;
+			}
+		}	
+
+		new_id++;
+		reader.close();
+
+		// Append new ID to file
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+		writer.write(Integer.toString(new_id) + "\n");
+		writer.close();
+
+		return new_id;
+	}
+
+	/*
+	 * Crée un nouveau fichier, retourne sans rien faire si le fichier existe déjà
+	 */
+	public boolean create(String name) throws Exception {
+		File file = new File("files/" + name);
+		if (file.exists()) {
+			return false;
+		}
+
+		file.createNewFile();
+		return true;
+	}
+
+	/*
+	 * Renvoie la liste des noms de fichiers avec le propriétaire du verrou si
+	 * tel est le cas
+	 */
+	public String list() throws Exception {
+		File folder = new File("files");
+		File[] list = folder.listFiles();	
+
+		String ret = "";
+		for (int i = 0; i < list.length; i++) {
+			if (list[i].isFile()) {
+				ret += list[i].getName() + " - Non verouillé\n";
+			}
+		}
+
+		return ret;
 	}
 }
