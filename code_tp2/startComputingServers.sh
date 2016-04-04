af233844fc1e882639a18294bed10ec90a97a8c5
pierluc <pier-luc.morissette@technowait.com>
2016-04-03 20:07:09 -0400

    Add a script that starts 5 computing servers.

---
+++ code_tp2/startComputingServers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo | ./computingServer Wolfgang 5025 2 0 &
+echo | ./computingServer Ludwig 5026 4 0 &
+echo | ./computingServer Chopin 5027 8 0 &
+echo | ./computingServer Bach 5028 16 3 &
+echo | ./computingServer Vivaldi 5029 32 6 &
