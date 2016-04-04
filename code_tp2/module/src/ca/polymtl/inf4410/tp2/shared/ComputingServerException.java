badad094ad6dfa862f95827f56f563253b028889
pierluc <pier-luc.morissette@technowait.com>
2016-04-03 20:07:42 -0400

    Add ComputingServerException class.

---
+++ code_tp2/module/src/ca/polymtl/inf4410/tp2/shared/ComputingServerException.java
@@ -0,0 +1,9 @@
+package ca.polymtl.inf4410.tp2.shared;
+
+public class ComputingServerException extends Exception {
+
+    public ComputingServerException(String message){
+        super(message);
+    }
+
+}
