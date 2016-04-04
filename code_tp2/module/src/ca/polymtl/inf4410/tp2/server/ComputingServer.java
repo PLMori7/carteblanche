d8d8fdcd07c69eb538714f0ae734688229014109
pierluc <pier-luc.morissette@technowait.com>
2016-04-03 20:08:13 -0400

    ComputingServer throws ComputingServerException instead of Exception.

--- code_tp2/module/src/ca/polymtl/inf4410/tp2/server/ComputingServer.java
+++ code_tp2/module/src/ca/polymtl/inf4410/tp2/server/ComputingServer.java
@@ -30,6 +30,7 @@ import java.nio.file.Paths;
 import ca.polymtl.inf4410.tp2.shared.ServerInterface;
 import ca.polymtl.inf4410.tp2.shared.Operations;
 import ca.polymtl.inf4410.tp2.shared.Operation;
+import ca.polymtl.inf4410.tp2.shared.ComputingServerException;
 
 public class ComputingServer implements ServerInterface {
 	private ComputingServerConfig serverConfig = null;
@@ -81,7 +82,7 @@ public class ComputingServer implements ServerInterface {
 
 			Registry registry = LocateRegistry.getRegistry("127.0.0.1", 5000);
 			registry.rebind(serverConfig.getName(), stub);
-			System.out.println("Server ready.");
+			System.out.println("Server " + serverConfig.getName() + " ready.");
 		} catch (ConnectException e) {
 			System.err
 					.println("Impossible de se connecter au registre RMI. Est-ce que rmiregistry est lancé ?");
@@ -100,7 +101,7 @@ public class ComputingServer implements ServerInterface {
 	public int handleTasks(ArrayList<Operation> pendingOperations) throws Exception {
 		int result = 0;
 		if (serverOverloaded(pendingOperations.size())) {
-			throw new Exception("Le serveur est trop chargé pour accepter cette tâche.");
+			throw new ComputingServerException("Le serveur est trop chargé pour accepter cette tâche.");
 		} else {
 			for(Operation operation: pendingOperations) {
 				System.out.println("Opération: " + operation.getType() + " " + operation.getOperand());
